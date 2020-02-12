bb09cdadff6b902e4857cff26281434b2f5f1680
Elkhan Mursali <elxanmursal@gmail.com>
2020-02-12 14:05:24 +0100

    Adding components

---
+++ src/app/tariffs/tariffs.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+
+import { TariffsComponent } from "./tariffs.component";
+
+describe("TariffComponent", () => {
+  let component: TariffsComponent;
+  let fixture: ComponentFixture<TariffsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [TariffsComponent]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TariffsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+});
